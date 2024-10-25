899562989704436d9ea50bb7884746414cf62a0c
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-10-25 12:43:27 -0400

    implement INFERNO_DISABLE_TLS_TEST env

--- lib/tls_test_kit/tls_version_test.rb
+++ lib/tls_test_kit/tls_version_test.rb
@@ -70,6 +70,7 @@ module TLSTestKit
     input :url
 
     run do
+      omit_if ENV['INFERNO_DISABLE_TLS_TEST'], "This test is disabled because INFERNO_DISABLE_TLS_TEST is present in env."
       skip_if url.blank?, "Could not verify when no url provided."
       
       uri = URI(url)
